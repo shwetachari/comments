42af886cab0e349ec6e16e67bfb3362461164352
Shweta Patrachari <shwetachari@gmail.com>
2017-04-08 09:54:54 -0700

    added delete all and delete last functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
-$('#submitForm').mousedown(function(){
-  var formText = document.getElementById("formText").value
-  var newLi = document.createElement("li")
-  var liDivName = document.createElement("div")
-  var liDivText = document.createElement("div")
-  var unorderedList = document.getElementById("unorderedList")
-  // appends node named newDiv to body
-  unorderedList.appendChild(newLi)
-  var name = document.getElementById("name").value
-  var liName = document.createTextNode(name)
-  var liText = document.createTextNode(formText)
-  liDivName.appendChild(liName)
-  liDivText.appendChild(liText)
-  newLi.appendChild(liDivName)
-  newLi.appendChild(liDivText)
-  liDivName.className = "liDivName"
-  liDivText.className = "liDivText"
-  newLi.className = "listEntry"
+$("#submitForm").mousedown(function(){
+  var newLi = document.createElement("li");
+  var liDivName = document.createElement("div");
+  var liDivText = document.createElement("div");
+  var unorderedList = document.getElementById("unorderedList");
+  // appends node newLi to unorderedList
+  unorderedList.appendChild(newLi);
+  var formText = document.getElementById("formText").value;
+  var name = document.getElementById("name").value;
+  var liName = document.createTextNode(name);
+  var liText = document.createTextNode(formText);
+  liDivName.appendChild(liName);
+  liDivText.appendChild(liText);
+  newLi.appendChild(liDivName);
+  newLi.appendChild(liDivText);
+  liDivName.className = "liDivName";
+  liDivText.className = "liDivText";
+  newLi.className = "listEntry";
+  document.getElementById("name").value = "";
+  document.getElementById("formText").value = "";
+})
+
+$("#deleteAll").click(function(){
+  var unorderedList = document.getElementById("unorderedList");
+  while (unorderedList.hasChildNodes()) {
+    unorderedList.removeChild(unorderedList.lastChild);
+  }
+})
+$("#deleteLast").click(function(){
+  var unorderedList = document.getElementById("unorderedList");
+  unorderedList.removeChild(unorderedList.lastChild);
 })
